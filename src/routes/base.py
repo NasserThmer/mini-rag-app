2bb56a7cbf9c7a1b47ea621118d6b8605b198f31
Nasser Thmer <nasserthmer1992@gmail.com>
2025-06-29 22:45:29 +0500

    fix: update welcome endpoint to use dependency injection for app settings

--- src/routes/base.py
+++ src/routes/base.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import FastAPI , APIRouter , Depends
 import os
 from helpers.config import get_settings
 
@@ -8,7 +8,7 @@ base_router = APIRouter(
 )
 
 @base_router.get("/")
-async def welcome():
+async def welcome(app_settings= Depends(get_settings)):
     app_settings = get_settings()
     app_name = app_settings.APP_NAME
     app_version = app_settings.APP_VERSION  # ✅ التصحيح هنا
