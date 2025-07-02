296b93f376d98838f2618e7eed024efcafeda1c4
Nasser Thmer <nasserthmer1992@gmail.com>
2025-07-02 23:08:57 +0500

    feat: enhance configuration and routing for file upload functionality

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI 
 from routes import base # Assuming base.py is in the routes directory
+from routes import data # Assuming data.py is in the routes directory
 
 
 app = FastAPI()
 app.include_router(base.base_router)
+app.include_router(data.data_router)
 # app.include_router(base_router)
---
+++ src/routes/data.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI , APIRouter , Depends , uploadFile
+import os
+from helpers.config import get_settings , Settings
+from controllers import DataController
+
+data_router = APIRouter(
+    prefix="/api/v1/data",
+    tags=["api_v1 , data"],
+)
+
+@data_router.post("/upload/{project_id}")
+
+async def upload_data(project_id: str, file: uploadFile, 
+                      app_settings: Settings = Depends(get_settings)):
+    
+
+    # validate the size file propersties 
+    is_valid= DataController().validate_upload_file(file=file)
+
+    return is_valid
