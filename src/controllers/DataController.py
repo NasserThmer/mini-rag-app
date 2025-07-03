a21613ecd4ec5f21afcbff6cd17e442712d26f79
Nasser Thmer <nasserthmer1992@gmail.com>
2025-07-03 13:16:51 +0500

    fix: update file validation method name and correct size scale in DataController

--- src/controllers/DataController.py
+++ src/controllers/DataController.py
@@ -4,8 +4,8 @@ from fastapi  import UploadFile
 class DataController(BaseController):
     def __init__(self):
         super().__init__()
-        self.size_scale = 1024 * 1024
-    def validate_upload_file(self, file : UploadFile):
+        self.size_scale = 1048576 
+    def validate_uploaded_file(self, file: UploadFile):
         if file.content_type not in self.app_settings.FILE_ALLOWED_TYPES:
             # raise ValueError(f"File type {file.content_type} is not allowed.")
             return False
